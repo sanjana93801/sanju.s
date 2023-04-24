cf016b19ef20f2bc4bd0225ca7d046ad1247b739
email <sanjanareddysanjana2@gmail.com>
2023-04-24 10:52:46 +0530

    in development branch i added some containt

---
+++ application.java
@@ -0,0 +1,7 @@
+Introducing Files, the ultimate file manager app for Windows. With its sleek and intuitive design, navigating through your files has never been easier. Files features tabs for easy switching between different folders, a column view for quick file browsing, and dual pane support for efficient file management. In addition, you can easily create and extract archives with just a few clicks, making file compression and decompression a dhfieug9
+
+
+
+sgdiewut9oue
+skf9erut9
+
